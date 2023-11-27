1b76d8b5c375ddb8900427fc8a1bd1a459314c69
John <john.t.singer@gmail.com>
2023-11-27 10:53:43 +0100

    add login commands

---
+++ epicevents/cli/commands/login.py
@@ -0,0 +1,47 @@
+import typer
+from typing_extensions import Annotated
+from datetime import datetime, timedelta, timezone
+from django.contrib.auth import authenticate
+from cli.utils.console import console
+from cli.utils.validators import validate_callback
+from cli.utils.token import NewToken
+
+
+app = typer.Typer()
+
+
+@app.callback(invoke_without_command=True)
+def login(
+    email: Annotated[
+        str,
+        typer.Option(
+            "--email",
+            "-e",
+            prompt=True,
+            help="Your email address",
+            callback=validate_callback
+        )
+    ],
+    password: Annotated[
+        str,
+        typer.Option(
+            "--password",
+            "-p",
+            prompt=True,
+            hide_input=True,
+            help='Your password'
+        )
+    ]
+):
+    user = authenticate(username=email, password=password)
+    if user is not None:
+        payload = {
+            'user_id': user.id,
+            'sub': user.get_full_name(),
+            'iss': 'epicevents_crm',
+            'exp': datetime.now(tz=timezone.utc) + timedelta(minutes=5)
+        }
+        NewToken(payload)
+        console.print('[green]Successfully logged in')
+    else:
+        console.print("[red]Wrong email or password.")
