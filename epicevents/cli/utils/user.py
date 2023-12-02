8aefd1fad3976614f6d1c4521cfa2fb5e83b5f3d
John <john.t.singer@gmail.com>
2023-12-02 14:50:36 +0100

    remove permissions_callback from user

--- epicevents/cli/utils/user.py
+++ epicevents/cli/utils/user.py
@@ -1,26 +1,9 @@
-import typer
 from jwt.exceptions import ExpiredSignatureError
 from django.contrib.auth import get_user_model
 from cli.utils.token import Token, TokenNotFoundError
-from cli.utils.console import console
 
 
 User = get_user_model()
-
-
-def permission_callback(ctx: typer.Context):
-    command_name = ctx.info_name
-    subcommand = ctx.invoked_subcommand
-
-    if command_name == 'collaborator':
-        command_name = 'user'
-    user = get_user()
-    if not user:
-        console.print('[red]Token has expired. Please log in again.')
-        exit()
-    if not user.has_perm(f'orm.{subcommand}_{command_name}'):
-        console.print('[red]You are not allowed.')
-        exit()
 
 
 def get_user():
