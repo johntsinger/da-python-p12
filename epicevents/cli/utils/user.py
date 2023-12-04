97c03d0bac9c0c4e08ba54f3638485cb57996b56
John <john.t.singer@gmail.com>
2023-12-04 10:40:38 +0100

    get user only by id

--- epicevents/cli/utils/user.py
+++ epicevents/cli/utils/user.py
@@ -1,5 +1,6 @@
 from jwt.exceptions import ExpiredSignatureError
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 from cli.utils.token import Token, TokenNotFoundError
 
 
@@ -7,19 +8,12 @@ User = get_user_model()
 
 
 def get_user():
+    """Get current authenticated user"""
     try:
         token = Token().decode
     except (ExpiredSignatureError, TokenNotFoundError):
         return None
-    if token['sub']:
-        first_name, last_name = token['sub'].split()
-    else:
-        user = User.objects.get(id=token['user_id'])
-        if not user.is_superuser:
-            return None
-        return user
-    return User.objects.get(
-        id=token['user_id'],
-        first_name=first_name,
-        last_name=last_name
-    )
+    try:
+        return User.objects.get(id=token['user_id'])
+    except ObjectDoesNotExist:
+        return None
