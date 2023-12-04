94e0f7dfa0da9146f6ed089797f4f90643472355
John <john.t.singer@gmail.com>
2023-12-04 21:23:19 +0100

    add help message

--- epicevents/cli/commands/cli.py
+++ epicevents/cli/commands/cli.py
@@ -12,25 +12,30 @@ from cli.utils.callbacks import permissions_callback
 app = typer.Typer()
 app.add_typer(
     login.app,
-    name='login'
+    name='login',
+    help="Login. Options are prompted if omitted."
 )
 app.add_typer(
     collaborator.app,
     name='collaborator',
-    callback=permissions_callback
+    callback=permissions_callback,
+    help="Access collaborator commands."
 )
 app.add_typer(
     client.app,
     name='client',
-    callback=permissions_callback
+    callback=permissions_callback,
+    help="Access client commands."
 )
 app.add_typer(
     contract.app,
     name='contract',
-    callback=permissions_callback
+    callback=permissions_callback,
+    help="Access contract commands."
 )
 app.add_typer(
     event.app,
     name='event',
-    callback=permissions_callback
+    callback=permissions_callback,
+    help="Access event commands."
 )
