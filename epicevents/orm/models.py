3c58402006bd633b0ce46420711aaadd66da4989
John <john.t.singer@gmail.com>
2023-12-19 19:51:03 +0100

    add compagny __str__ function, add full_clean method in Client models' save method

--- epicevents/orm/models.py
+++ epicevents/orm/models.py
@@ -94,6 +94,9 @@ class Compagny(models.Model):
         unique=True
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Client(models.Model):
     first_name = models.CharField(max_length=50)
@@ -112,6 +115,10 @@ class Client(models.Model):
     )
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -170,6 +177,3 @@ class Event(models.Model):
     def save(self, *args, **kwargs):
         self.full_clean()
         return super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f'{self.name}'
