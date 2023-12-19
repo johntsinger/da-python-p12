425c51bc483b686c3d03f14b6df8612c441d7690
John <john.t.singer@gmail.com>
2023-12-19 23:44:42 +0100

    remove .env.test only if it exists

--- epicevents/epicevents/runner.py
+++ epicevents/epicevents/runner.py
@@ -5,4 +5,5 @@ from django.test.runner import DiscoverRunner
 class MyTestRunner(DiscoverRunner):
     def teardown_test_environment(self, **kwargs):
         super(MyTestRunner, self).teardown_test_environment(**kwargs)
-        os.remove('.env.test')
+        if os.path.exists('.env.test'):
+            os.remove('.env.test')
