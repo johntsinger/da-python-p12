87b4ccd631b1b9df0967670e4a61b15dc9589afa
John <john.t.singer@gmail.com>
2023-12-19 19:42:55 +0100

    update test to use ctx for default value

--- epicevents/cli/tests/unit_tests/utils/test_prompt.py
+++ epicevents/cli/tests/unit_tests/utils/test_prompt.py
@@ -3,8 +3,20 @@ from django.test import TestCase
 from cli.utils.prompt import prompt_for
 
 
+class Obj:
+    def __init__(self):
+        self.phone = '+33600000000'
+
+
+class Context:
+    def __init__(self):
+        self.obj = Obj()
+
+
 class TestPromptFor(TestCase):
+    ctx = Context()
+
     @patch('typer.prompt', return_value='0600000000')
     def test_prompt_for(self, mock_typer_prompt):
-        value = prompt_for('test', validator_name='phone')
+        value = prompt_for('phone', self.ctx)
         self.assertEqual(value, '+33600000000')
