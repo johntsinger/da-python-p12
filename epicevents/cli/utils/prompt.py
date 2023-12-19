f10432fd40372356f9759848d9f41191adf0b2b9
John <john.t.singer@gmail.com>
2023-12-19 19:42:14 +0100

    update prompt_for to use default value

--- epicevents/cli/utils/prompt.py
+++ epicevents/cli/utils/prompt.py
@@ -1,11 +1,23 @@
 import typer
+from datetime import datetime
 from cli.utils.validators import validate_value
 
 
-def prompt_for(message, validator_name=None, ctx=None):
+def prompt_for(field_name, ctx):
     new_value = None
+    if field_name == 'department':
+        default = getattr(ctx.obj, 'groups').first()
+    else:
+        default = getattr(ctx.obj, field_name)
+    if isinstance(default, datetime):
+        default = default.strftime('%d-%m-%Y %H:%M')
     while new_value is None:
-        new_value = typer.prompt(message.capitalize())
-        if validator_name:
-            new_value = validate_value(validator_name, new_value, ctx)
+        if field_name == 'password':
+            new_value = typer.prompt(field_name.capitalize())
+        else:
+            new_value = typer.prompt(
+                field_name.capitalize(),
+                default=str(default)
+            )
+        new_value = validate_value(field_name, new_value, ctx)
     return new_value
