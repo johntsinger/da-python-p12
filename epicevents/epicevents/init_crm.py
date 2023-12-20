8a81d0e48f09adf02ab2c784d8c91ace1dc8c394
John <john.t.singer@gmail.com>
2023-12-20 11:36:20 +0100

    rework init_crm

--- epicevents/epicevents/init_crm.py
+++ epicevents/epicevents/init_crm.py
@@ -1,15 +1,17 @@
 import os
 from pathlib import Path
+from dotenv import set_key, dotenv_values
 from django.core.management.utils import get_random_secret_key
 
 
 def generate_secret_key(base_dir='', file_name='.env'):
     base_dir = Path(base_dir)
     if not Path.is_file(base_dir / file_name):
-        with open(base_dir / file_name, 'w') as file:
-            secret_key = get_random_secret_key()
-            file.write(
-                f"SECRET_KEY={secret_key}"
-            )
-            os.environ['SECRET_KEY'] = secret_key
-    return secret_key
+        with open(base_dir / file_name, 'w'):
+            pass
+    values = dotenv_values(base_dir / file_name)
+    if not values.get('SECRET_KEY'):
+        key = get_random_secret_key()
+        set_key(file_name, 'SECRET_KEY', key)
+        os.environ['SECRET_KEY'] = key
+        return key
